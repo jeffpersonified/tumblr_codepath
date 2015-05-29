77de7dd942da3ca5fbbea5f0cbc97d8ab11c251d
jeffpersonified <jeff.matthew.smith@gmail.com>
2015-05-28 21:21:06 -0700

    Create initial TabBarViewController view

---
+++ Tumblr/TabBarViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarViewController.swift
+//  Tumblr
+//
+//  Created by Jeff Smith on 5/28/15.
+//  Copyright (c) 2015 Jeff Smith. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: NSObject {
+   
+}
--- Tumblr/ViewController.swift
+++ Tumblr/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tabBar: UIView!
+    @IBOutlet weak var contentView: UIView!
+    
+    let tabBackgroundColor = UIColor(red: 0.19, green: 0.26, blue: 0.34, alpha: 1)
+    let contentBackgroundColor = UIColor(red: 0.19, green: 0.27, blue: 0.36, alpha: 1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tabBar.backgroundColor = tabBackgroundColor
+        contentView.backgroundColor = contentBackgroundColor
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
-
