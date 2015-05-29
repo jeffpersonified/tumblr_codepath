d1640b17a9736f999e0e6c5615b8357c57ad922f
Jeff Smith <jeff.matthew.smith@gmail.com>
2015-05-29 11:54:46 -0700

    Add white status bar

--- Tumblr/TabBarViewController.swift
+++ Tumblr/TabBarViewController.swift
@@ -2,12 +2,26 @@
 //  TabBarViewController.swift
 //  Tumblr
 //
-//  Created by Jeff Smith on 5/28/15.
+//  Created by Jeff Smith on 5/29/15.
 //  Copyright (c) 2015 Jeff Smith. All rights reserved.
 //
 
 import UIKit
 
-class TabBarViewController: NSObject {
-   
+class TabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
 }
